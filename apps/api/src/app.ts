5bdd3a7071696427d5ea9836f52219aff9074f15
code-env <org.codeen@gmail.com>
2025-12-02 09:59:18 +0100

    feat: added the cors and env package to the /api

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,16 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { env } from "@skaleam/env/server";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
+  .use(
+    cors({
+      origin: (request) => {
+        const origin = request.headers.get("origin") ?? "";
+        return env.TRUSTED_ORIGINS.includes(origin);
+      },
+    })
+  )
   .get("/", () => ({
     message: "Welcome to Skale AM API",
     version: "0.0.1",
@@ -14,9 +24,21 @@ const app = new Elysia()
     userId: params.id,
     message: `User ${params.id} retrieved`,
   }))
-  .post("/json", ({ body }) => ({
-    received: body,
-  }));
+  .post(
+    "/user",
+    ({ body: { age, name } }) => {
+      return {
+        name,
+        age,
+      };
+    },
+    {
+      body: t.Object({
+        name: t.String(),
+        age: t.Number(),
+      }),
+    }
+  );
 
 export type App = typeof app;
 
